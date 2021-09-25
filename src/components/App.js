ffa2d77bcc181c3681bdc8a84231e2f7f995e998
pavlvs <pavlvsxavier@gmail.com>
2021-09-25 16:04:42 +0100

    handling form submital

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import SearchBar from './SearchBar'
 
 class App extends React.Component {
     state = {}
@@ -6,7 +7,7 @@ class App extends React.Component {
     render() {
         return (
             <div className='ui container'>
-                Change Me
+                <SearchBar />
             </div>
         )
     }
