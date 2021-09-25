ae0e70ebeb13b5d1d7b746d2a2c55967bec43e5e
pavlvs <pavlvsxavier@gmail.com>
2021-09-25 17:51:42 +0100

    added youtube custom axios

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,13 @@
+import axios from 'axios'
+
 const KEY = 'AIzaSyBZTGf13FNHUVclFTqMP2Kki3hTApsxUpk'
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        type: 'video',
+        maxResults: 5,
+        key: KEY,
+    },
+})
