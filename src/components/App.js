763ddae2788c1c3d5ffad81b3de8378dddbce8fc
pavlvs <pavlvsxavier@gmail.com>
2021-09-25 14:48:42 +0100

    added files to project scaffold

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class App extends React.Component {
+    state = {}
+
+    render() {
+        return (
+            <div className='ui container'>
+                Change Me
+            </div>
+        )
+    }
+}
+
+export default App
