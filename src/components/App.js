78ea93a4025d1176b91005043cd27d3a63498af6
pavlvs <pavlvsxavier@gmail.com>
2021-09-26 10:15:05 +0100

    updated state with fetched data

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,25 @@ import youtube from '../apis/youtube'
 import SearchBar from './SearchBar'
 
 class App extends React.Component {
-    state = {}
+    state = {
+        videos: [],
+    }
 
-    onTermSubmit = (term) => {
-        youtube.get('/search', {
+    onTermSubmit = async (term) => {
+        const response = await youtube.get('/search', {
             params: {
                 q: term,
             },
         })
+
+        this.setState({ videos: response.data.items })
     }
 
     render() {
         return (
             <div className='ui container'>
-                <SearchBar onFormSubmit={this.onTermSubmit} />
+                <SearchBar onFormSubmit={this.onTermSubmit} />I have{' '}
+                {this.state.videos.length} videos.
             </div>
         )
     }
